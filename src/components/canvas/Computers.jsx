af710489c991d12b7bab02c6865b3b8cb7ae240f
shruti-panchal-20 <shrutikpanchal20@gmail.com>
2025-09-05 22:28:37 +0530

    glb file updated

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -5,26 +5,12 @@ const ComputersCanvas = () => {
     <div className="w-full h-screen flex justify-center items-center bg-black">
       <model-viewer
         src="/models/scene.glb"
-        poster="/models/poster.webp"
         ar
         ar-modes="webxr scene-viewer quick-look"
         camera-controls
         auto-rotate
-        tone-mapping="neutral"
-        shadow-intensity="1"
-        exposure="1"
-        reveal="auto"
-        loading="lazy"
         style={{ width: "100%", height: "100%", outline: "none" }}
-      >
-        <div className="progress-bar hide" slot="progress-bar">
-          <div className="update-bar"></div>
-        </div>
-
-        <button slot="ar-button" id="ar-button">
-          View in your space
-        </button>
-      </model-viewer>
+      ></model-viewer>
     </div>
   );
 };
