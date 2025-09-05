e31eccaea7b937f572f6000113dc26e3fb01eb9f
shruti-panchal-20 <shrutikpanchal20@gmail.com>
2025-09-05 22:49:07 +0530

    Navbar Updated

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,17 +1,22 @@
 import "@google/model-viewer";
+import { Suspense } from "react";
+
+import CanvasLoader from "../Loader";
 
 const ComputersCanvas = () => {
   return (
-    <div className="w-full h-screen flex justify-center items-center bg-black">
-      <model-viewer
-        src="/models/scene.glb"
-        ar
-        ar-modes="webxr scene-viewer quick-look"
-        camera-controls
-        auto-rotate
-        style={{ width: "100%", height: "100%", outline: "none" }}
-      ></model-viewer>
-    </div>
+    <Suspense fallback={<CanvasLoader />}>
+      <div className="w-full h-screen flex justify-center items-center ">
+        <model-viewer
+          src="/models/scene.glb"
+          ar
+          ar-modes="webxr scene-viewer quick-look"
+          camera-controls
+          auto-rotate
+          style={{ width: "100%", height: "100%", outline: "none" }}
+        ></model-viewer>
+      </div>
+    </Suspense>
   );
 };
 
